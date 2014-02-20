120931d39a8226dfea1c934349a26255d71b250f
lgustavolol <lgustavo.lol@gmail.com>
2014-02-20 17:07:15 -0300

    RMTools.Lib was added

---
+++ RMTools.Lib/MenuHandler.cs
@@ -0,0 +1,25 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMTools.Lib
+{
+  public class MenuHandler
+  {
+      private Commands2 Commands { get; set; }
+
+      public MenuHandler()
+      {
+          this.Commands = RMTSession.ApplicationContext.CommandBars;
+      }
+
+      private void CreateMainMenu()
+      {
+          //Command command = commands.AddNamedCommand2(_addInInstance, "RMTools", "RMTools", "Executes the command for RMTools", true, 59, ref contextGUIDS, (int)vsCommandStatus.vsCommandStatusSupported + (int)vsCommandStatus.vsCommandStatusEnabled, (int)vsCommandStyle.vsCommandStylePictAndText, vsCommandControlType.vsCommandControlTypeButton);
+      }
+  }
+}
---
+++ RMTools.Lib/RMTSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using EnvDTE80;
+
+namespace RMTools.Lib
+{
+  public static class RMTSession
+  {
+    public static DTE2 ApplicationContext { get; set; }
+    public static AddIn AddInInstanceContext { get; set; }
+  }
+}
